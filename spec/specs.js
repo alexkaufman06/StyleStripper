d8e6be0ddaede85e3195aa9bf5914fcd49d98100
Alex Kaufman <alexkaufman06@gmail.com>
2016-03-21 20:55:03 -0700

    Add specs for removing first part of inline style

--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,11 @@ describe("StyleStripper", function() {
     expect(true).to.eql(true);
   });
 
+  it("Will not remove text without a style", function() {
+    expect(removeStyles("dog")).to.eql("dog");
+  })
+
+  it("Will remove the beginning of an inline style", function() {
+    expect(removeStyles("<p style=\"\"></p>")).to.eql("<p \"></p>");
+  });
 });
